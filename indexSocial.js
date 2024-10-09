29c50352fb68cd8bf5060a421e253cecf2c80e5d
ZAMBUR <136861183+SNABUR@users.noreply.github.com>
2024-10-08 22:54:01 -0500

    Update indexSocial.js

--- indexSocial.js
+++ indexSocial.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const axios = require('axios'); // Importa axios
 require("dotenv").config();
 const app = express();
+const publicClient =require('./client');
 app.use(express.json());
 
 app.use(cors({
@@ -18,11 +19,11 @@ mongoose.connect(process.env.CREDENTIALS_MONGO_DB);
 const userSchema = new mongoose.Schema({
     walletAddress: { type: String, unique: true, required: true },  // Asegura que cada walletAddress sea única
     username: { type: String, default: 'Anonymous' },  // Puedes agregar más campos en el futuro
-    profileImage: { type: String, default: '' }  // Imagen de perfil (opcional)
+    profileImage: { type: String, default: '' },  // Imagen de perfil (opcional)
+    nonce: { type: String } // Agregar el campo nonce
 });
 
 const User = mongoose.model('User', userSchema);
-
 
 const commentSchema = new mongoose.Schema({
     text: String,
@@ -39,40 +40,15 @@ commentSchema.index({ tableName: 1, chainNet: 1 });
 
 const Comment = mongoose.model('Comment', commentSchema);
 
-// Crear un nuevo usuario
-app.post('/users', async (req, res) => {
-    const { walletAddress } = req.body;
-
+//getting coinbase balance
+const getCoinbaseBalance = async (address) => {
     try {
-        // Verifica si el usuario ya existe
-        let user = await User.findOne({ walletAddress });
-        if (!user) {
-            user = new User({ walletAddress});
-            await user.save();
-        }
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear o obtener el usuario' });
-    }
-});
-
-// Obtener un usuario por dirección de wallet
-app.get('/users/:walletAddress', async (req, res) => {
-    const { walletAddress } = req.params;
-    try {
-        // Busca el usuario en la base de datos
-        const user = await User.findOne({ walletAddress });
-        if (!user) {
-            return res.status(404).json({ message: 'Usuario no encontrado' });
-        }
-
-        // Hacer una llamada a la API de Coinbase para obtener el balance del usuario
-        const coinbaseResponse = await axios.post('https://api.developer.coinbase.com/rpc/v1/base-sepolia/yCYGyekgTfIGKsj-ZM_MQnJmbufDhUMh', {
+        const response = await axios.post('https://api.developer.coinbase.com/rpc/v1/base/yCYGyekgTfIGKsj-ZM_MQnJmbufDhUMh', {
             jsonrpc: "2.0",
             id: 1,
             method: "cdp_listBalances",
             params: [{
-                address: walletAddress,
+                address: address,
                 pageToken: "",
                 pageSize: 12
             }]
@@ -82,29 +58,113 @@ app.get('/users/:walletAddress', async (req, res) => {
             }
         });
 
-        // Devuelve el usuario y el balance de la API de Coinbase
-        res.json({ user, BaseBalances: coinbaseResponse.data });
+        return response.data; // Return the response data
     } catch (error) {
-        console.error("Error al obtener el usuario o balance:", error);
-        res.status(500).json({ message: 'Error al obtener el usuario o balance' });
+        console.error("Error fetching Coinbase balance:", error);
+        throw new Error('Error fetching Coinbase balance');
+    }
+};
+
+const verifySignature = async (nonce, signature, walletAddress) => {
+
+    const validSigner = await publicClient.verifyMessage({
+        address: walletAddress,
+        message: nonce,
+        signature,
+        })
+    return validSigner;
+};
+
+app.get('/generateNonce/:walletAddress', async (req, res) => {
+    const { walletAddress } = req.params;
+    const nonce = Math.floor(Math.random() * 1000000).toString(); // Generate a random nonce
+
+    try {
+        // Check if the user exists in the database
+        let user = await User.findOne({ walletAddress });
+
+        if (!user) {
+            // If user does not exist, create a new user with the nonce
+            user = new User({ walletAddress, nonce });
+            await user.save();
+        } else {
+            // If user exists, update the nonce
+            user.nonce = nonce;
+            await user.save();
+        }
+
+        // Return the nonce to the frontend for signing
+        res.json({ nonce });
+    } catch (error) {
+        console.error('Error generating nonce:', error);
+        res.status(500).json({ message: 'Error generating nonce' });
     }
 });
 
+// Autentificar nonce
+app.post('/authenticate', async (req, res) => {
+    const { walletAddress, signature, type } = req.body;
+    
+    // Check if walletAddress is provided
+    if (!walletAddress) {
+        return res.status(400).json({ error: "walletAddress is required" });
+    }
 
+
+    try {
+        // Find the user by wallet address
+        const user = await User.findOne({ walletAddress });
+
+        if (!user) {
+            return res.status(400).json({ message: 'User does not exist' });
+        }
+
+        if (await verifySignature(user.nonce, signature, walletAddress, type)) {
+            // If signature is valid, authentication is successful
+            let coinbaseData = [];
+            try {
+                coinbaseData = await getCoinbaseBalance(walletAddress);
+            } catch (err) {
+                console.error('Failed to fetch Coinbase balance:', err);
+                coinbaseData = []; // If it fails, return an empty array
+            }
+            // Fetch Coinbase balance
+
+            // Devuelve el usuario y el balance de la API de Coinbase
+            res.json({ message: 'Authentication successful', walletAddress, user, BaseBalances: coinbaseData });
+        } else {
+            res.status(401).json({ message: 'Invalid signature' });
+        }
+    } catch (error) {
+        console.error("Error during authentication:", error);
+        res.status(500).json({ message: 'Error during authentication' });
+    }
+});
 
 ///////comments/////////
-
 // Ruta para agregar un nuevo comentario con tableName, chainNet, y walletAddress
 app.post('/comments', async (req, res) => {
-    const { text, media, tableName, chainNet, walletAddress } = req.body;
+    const { text, media, tableName, chainNet, walletAddress, nonce } = req.body;
 
     try {
         // Buscar el usuario por su dirección de wallet
-        const user = await User.findOne({ walletAddress });
+        let user = await User.findOne({ walletAddress });
         if (!user) {
-            return res.status(404).json({ message: 'Usuario no encontrado' });
+            user = new User({ walletAddress });
+            await user.save();
         }
-
+        
+        // Verifica el nonce
+        if (!user.nonce) {
+            // Si no hay nonce, genera uno nuevo
+            const newNonce = Math.floor(Math.random() * 1000000).toString();
+            await User.updateOne({ walletAddress }, { $set: { nonce: newNonce } });
+            return res.status(400).json({ message: 'Nonce not found, a new one has been generated. Please retry with the new nonce.', newNonce });
+        }
+        // Verifica el nonce
+        if (user.nonce !== nonce) {
+            return res.status(400).json({ message: 'Invalid nonce' });
+        }
         // Calcular la fecha actual en el backend
         const date = Math.floor(Date.now());  // Timestamp Unix en segundos
 
@@ -118,12 +178,15 @@ app.post('/comments', async (req, res) => {
             walletAddress,  // Guarda la dirección de la wallet con el comentario
         });
         await newComment.save();
+
+        await User.updateOne({ walletAddress }, { $unset: { nonce: "" } });
+
         res.json(newComment);
     } catch (error) {
+        console.error('Error al crear el comentario o el usuario:', error);
         res.status(500).json({ message: 'Error al crear el comentario' });
     }
 });
-
 
 // Ruta para obtener comentarios filtrados por tableName y chainNet
 app.get('/comments', async (req, res) => {
@@ -145,7 +208,6 @@ app.get('/comments', async (req, res) => {
         res.status(500).json({ message: 'Error al obtener los comentarios' });
     }
 });
-
 
 // Iniciar el servidor
 app.listen(5000, () => {
